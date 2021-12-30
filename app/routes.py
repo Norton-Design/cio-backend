224caac5085535769e263200e844ab5a7abade59
Michael Norton <nortonkmichael@gmail.com>
2021-12-29 20:22:57 -0500

    finish routes

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,93 @@
 from app import app
 from datastore import DataStore
+from flask import jsonify
+from flask import request
+from werkzeug.http import HTTP_STATUS_CODES
+from datastore import DataStore
+import time
 
-@app.route("/")
-def hello_world():
-    return "hello, world"
+store = DataStore()
 
-# Remove after testing...
-# store = DataStore()
-# print(store.read_events_by_user_id(1))
-# print(store.events[1])
-# print(store.read_users_by_user_id(1))
+@app.route('/customers', methods=['GET'])
+def get_customers():
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 25, type=int)
+    customers = store.paginated_collection(page, per_page)
+    total = store.get_user_total()
+    if customers:
+        response = {
+            'customers' : customers,
+            'meta' : {
+                'page': page,
+                'per_page': per_page,
+                'total': total
+            }
+        }
+        return jsonify(response)
+    else:
+        return error_response(404, 'customers not found')
+
+
+@app.route('/customers/<id>', methods=['GET'])
+def get_customer(id):
+    customer = store.find_user_by_id(int(id))
+    if customer:
+        return jsonify({'customer': customer})
+    else:
+        return error_response(404, 'customer not found')
+
+@app.route('/customers/<id>', methods=['DELETE'])
+def delete_customer(id):
+    customer = store.find_user_by_id(int(id))
+    if customer & store.delete_user_by_id(int(id)):
+        payload = {'message': 'customer deleted by user_id'}
+        response = jsonify(payload)
+        response.status_code = 201
+        return response
+    else:
+        return error_response(404, 'customer not found')
+
+@app.route('/customers', methods=['POST'])
+def create_customer():
+    data = request.get_json()
+    potential_customer = data['customer']
+    user_id = potential_customer['id']
+    user_data = potential_customer['attributes']
+    if not store.find_user_by_id(user_id):
+        store.create_user(user_data, user_id)
+        customer = store.find_user_by_id(user_id)
+        if customer:
+            return jsonify({'customer': customer})
+    else:
+        return bad_request("customer already exists")
+
+@app.route('/customers/<id>', methods=['PATCH'])
+def update_customer(id):
+    customer = store.find_user_by_id(int(id))
+    if customer:
+        body = request.get_json()
+        attrs = body['customer']['attributes']
+        update_data = {
+            'data': attrs,
+            'user_id': id,
+            'timestamp': int(time.time())
+        }
+        updated_customer = store.update_user_attributes(update_data)
+        return jsonify({'customer': updated_customer})
+    else:
+        return bad_request("customer doesn't exist")
+
+
+def bad_request(message):
+    return error_response(400, message)
+
+def error_response(code, message=None):
+    payload = {
+        'error': HTTP_STATUS_CODES.get(code, 'Unknown error')
+    }
+    if message:
+        payload['message'] = message
+    response = jsonify(payload)
+    response.status_code = code
+    return response
+
--- datastore/__init__.py
+++ datastore/__init__.py
@@ -1,10 +1,14 @@
 import json
+import time
 
 class DataStore:
     def __init__(self):
         self.events = {}
         self.users = {}
+        self.user_total = 0
+        self.next_available_user_id = 1
         self.create_datastore()
+        self.set_next_available_user_id()
 
     def create_datastore(self):
         with open('./datastore/data/messages.1.data', 'r') as f:
@@ -12,17 +16,13 @@ class DataStore:
             for line in lines:
                 parsed_line = json.loads(line)
                 if parsed_line['type'] == 'event':
-                    self.handle_event(parsed_line)
+                    self.screen_pathological_events(parsed_line)
                 elif parsed_line['type'] == 'attributes':
-                    self.handle_attributes(parsed_line)
+                    self.update_user_attributes(parsed_line)
     
-    def handle_event(self, event_message):
-        pathological_inputs = []
-        if 'user_id' not in event_message:
-            # What to do with this edgecase??
-            pathological_inputs.append(event_message)
-        else:
-            return self.add_event(event_message)
+    def screen_pathological_events(self, event_message):
+        if 'user_id' in event_message:
+            self.add_event(event_message)
 
     def add_event(self, event_message):
         # This is an ugly function...
@@ -44,12 +44,11 @@ class DataStore:
             event_set.add(event_id)
             self.events[user_id] = {}
             self.events[user_id][event_name] = event_set
-                
 
-    def handle_attributes(self, attribute_message):
-        user_id = int(attribute_message['user_id'])
-        message_timestamp = attribute_message['timestamp']
-        data = attribute_message['data']
+    def update_user_attributes(self, user_data):
+        user_id = int(user_data['user_id'])
+        message_timestamp = user_data['timestamp']
+        data = user_data['data']
         
         if user_id in self.users:
             # check data and modify
@@ -65,16 +64,9 @@ class DataStore:
                     curr_attrs[key] = (value, message_timestamp)
             if  message_timestamp > curr_timestamp:
                 curr_user['last_updated'] = message_timestamp
+            return self.find_user_by_id(user_id)
         else: 
-            parsed_data = {}
-            for key, value in data.items():
-                parsed_data[key] = (value, message_timestamp)
-            new_user = {
-                "id": user_id,
-                "last_updated": message_timestamp,
-                "attributes": parsed_data
-            }
-            self.users[user_id] = new_user
+            self.create_user(data, user_id, message_timestamp)
 
     def most_recent_attribute(self, curr_attr_pair, new_attr_pair):
         if curr_attr_pair[1] > new_attr_pair[1]:
@@ -82,7 +74,7 @@ class DataStore:
         else:
             return new_attr_pair
 
-    def read_events_by_user_id(self, user_id):
+    def find_events_by_user_id(self, user_id):
         if user_id in self.events:
             formatted_user_events = {}
             user_events = self.events[user_id]
@@ -90,11 +82,9 @@ class DataStore:
                 formatted_user_events[key] = len(user_events[key])
             return formatted_user_events
         else:
-            print('user_id not found in EVENTS, handle error here <-------')
-            return None
+            return {}
 
-
-    def read_users_by_user_id(self, user_id):
+    def find_user_by_id(self, user_id):
         if user_id in self.users:
             formatted_user = {}
             datastore_user = self.users[user_id]
@@ -107,14 +97,54 @@ class DataStore:
                     formatted_user['attributes'] = parsed_attrs                    
                 else:
                     formatted_user[key] = value
-            user_events = self.read_events_by_user_id(user_id)
-            if user_events:
-                formatted_user['events'] = user_events
+            user_events = self.find_events_by_user_id(user_id)
+            formatted_user['events'] = user_events
             return formatted_user
         else:
-            print('user_id not found in USERS, handle error here <-------')
             return None
 
+    def set_next_available_user_id(self):
+        count = self.next_available_user_id
+        while count in self.users:
+            count += 1
+        self.next_available_user_id = count
+
+    def delete_user_by_id(self, user_id):
+        user = self.users.pop(user_id, None)
+        events = self.events.pop(user_id, None)
+        self.user_total -= 1
+        return user or events 
+
+    def create_user(self, user_data, user_id=None, timestamp=None):
+        if not user_id:
+            user_id = self.next_available_user_id
+        if not timestamp:
+            timestamp = int(time.time())
+        parsed_data = {}
+        for key, value in user_data.items():
+            parsed_data[key] = (value, timestamp)
+        new_user = {
+            "id": user_id,
+            "last_updated": timestamp,
+            "attributes": parsed_data
+        }
+        self.users[user_id] = new_user
+        self.user_total += 1
+        return self.users[user_id]
+
+
+    def get_user_total(self):
+        return self.user_total
+
+    def paginated_collection(self, page, per_page):
+        starting_id = (page - 1) * per_page + 1
+        ending_id = page * per_page
+        user_collection = []
+        for n in range(starting_id, ending_id + 1):
+            user = self.find_user_by_id(n)
+            if user:
+                user_collection.append(user)
+        return user_collection
 
 
 
